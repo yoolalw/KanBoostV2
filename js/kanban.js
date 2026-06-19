eb79c12f7ff97eedcbda05488a6b19ff37d996b9
yoolalw <larakemilyprofissional@gmail.com>
2026-06-19 12:04:15 -0300

    Modificando tamanho dos cards

--- js/kanban.js
+++ js/kanban.js
@@ -12,12 +12,12 @@ const done = document.getElementById('done')
 
 JSON.parse(localStorage.ativ).forEach(a => {
     const card = document.createElement('div')
-    card.className = "atividade h-[120px] z-5 rounded-xl bg-white shadow-lg w-full flex justify-center border-2 border-gray-300  align-middle items-center m-auto flex-col"
+    card.className = "atividade h-[140px] z-5 rounded-xl bg-white shadow-lg w-full flex justify-center border-2 border-gray-300  align-middle items-center m-auto flex-col"
     card.innerHTML += `
-        <h1 class="text-[30px] text-[#333]">${a}</h1>
+        <h1 class="text-[30px]  text-[#333]">${a}</h1>
         <div class="buttons flex gap-15 mt-5">
-            <button id="voltar" class="voltar bg-blue-100 text-[15px] text-blue-800 border-blue-300  border-2 h-[30px] cursor-pointer rounded-full hover:bg-blue-300  w-[75px]">Voltar</button>
-            <button id="avancar" class="avancar bg-blue-100 text-[15px] text-blue-800 border-blue-300 h-[30px] border-2 cursor-pointer  rounded-full hover:bg-blue-300 w-[75px]">Avançar</button>
+            <button id="voltar" class="voltar bg-gradient-to-r from-blue-100 to-blue-200 text-[15px] text-blue-800 border-blue-300  border-2 h-[30px] cursor-pointer rounded-full hover:bg-blue-300  w-[75px]">Voltar</button>
+            <button id="avancar" class="avancar bg-gradient-to-l from-blue-100 to-blue-200 text-[15px] text-blue-800 border-blue-300 h-[30px] border-2 cursor-pointer  rounded-full hover:bg-blue-300 w-[75px]">Avançar</button>
         </div>
     `
     todo.appendChild(card)
