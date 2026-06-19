709d993f4163c616d8ecb3e3f86fc9da23432e62
yoolalw <larakemilyprofissional@gmail.com>
2026-06-19 10:08:04 -0300

    Mensagem de erro caso campo esteja vazio na pagina de cadastro de tarefas

--- js/register.js
+++ js/register.js
@@ -2,18 +2,29 @@ function home() {
     window.location.href = "homepage.html"
 }
 
+const msg = document.getElementById('msg')
+msg.style.display = "none"
+
 var arr = []
 const button = document.getElementById('button')
 button.addEventListener('click', (e) => {
     e.preventDefault()
 
-    if(localStorage.ativ){
-        arr = JSON.parse(localStorage.ativ)
+    if (document.getElementById('input').value == "") {
+        msg.style.display = "block"
+        msg.textContent = "Campo de cadastro vazio! Preencha-o e tente novamente."
+    } else {
+
+        if (localStorage.ativ) {
+            arr = JSON.parse(localStorage.ativ)
+        }
+        const input = document.getElementById('input').value
+        arr.push(input)
+        document.getElementById('input').value = ""
+        localStorage.ativ = JSON.stringify(arr)
     }
-    const input = document.getElementById('input').value
-    arr.push(input)
-    document.getElementById('input').value = ""
-    localStorage.ativ = JSON.stringify(arr)
 })
+
+
 
 console.log(JSON.parse(localStorage.ativ))
