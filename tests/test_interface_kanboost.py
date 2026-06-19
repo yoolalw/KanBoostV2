06c40698fbc6eb55e857f67e6cdf71608cf267c7
yoolalw <larakemilyprofissional@gmail.com>
2026-06-19 11:32:20 -0300

    Adicionando time.sleep() em todos os testes

--- tests/test_interface_kanboost.py
+++ tests/test_interface_kanboost.py
@@ -25,13 +25,19 @@ class TestPages:
 
     # 1
     def test_displayed_items_in_homepage(self):
+        time.sleep(3)
+
         self.chrome.find_element(By.ID, "regBtn").is_displayed()
+        time.sleep(1)
         self.chrome.find_element(By.ID, "relBtn").is_displayed()
+        time.sleep(1)
         self.chrome.find_element(By.ID, "kanBtn").is_displayed()
 
     # 2
     def test_click_in_register_button(self):
+        time.sleep(3)
         self.chrome.find_element(By.ID, "regBtn").click()
+        time.sleep(1)
         WebDriverWait(self.chrome, 4).until(
             ec.url_to_be('http://127.0.0.1:5500/registerPage.html')
         )
@@ -39,22 +45,29 @@ class TestPages:
     # 3
     def test_error_msg(self):
         self.chrome.get('http://127.0.0.1:5500/registerPage.html')
+        time.sleep(3)
         self.chrome.find_element(By.ID, 'button').click()
+        time.sleep(1)
         WebDriverWait(self.chrome, 10).until(
             ec.visibility_of_element_located((By.ID, 'msg'))
         )
+        time.sleep(1)
         msg = self.chrome.find_element(By.ID, 'msg')
         assert msg.text == 'Campo de cadastro vazio! Preencha-o e tente novamente.'
 
     # 4
     def test_inserting_itens_in_field(self):
         self.chrome.get('http://127.0.0.1:5500/registerPage.html')
+        time.sleep(3)
         self.chrome.find_element(By.ID, 'input').send_keys('Atividade 1')
+        time.sleep(1)
         self.chrome.find_element(By.ID, 'button').click()
 
     # 5
     def test_clicking_in_kanban_button(self):
+        time.sleep(3)
         self.chrome.find_element(By.ID, 'kanBtn').click()
+        time.sleep(1)
         WebDriverWait(self.chrome, 10).until(
             ec.url_to_be('http://127.0.0.1:5500/kanban.html')
         )
@@ -62,13 +75,14 @@ class TestPages:
     # 6
     def test_verifyng_if_ativ_has_been_created(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
+        time.sleep(3)
         atividade = self.chrome.find_element(By.CLASS_NAME, "atividade")
         assert atividade.text == 'Atividade 1\nVoltar\nAvançar'
 
     # 7
     def test_moving_ativ_to_doing_column(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
         time.sleep(1)
@@ -76,7 +90,7 @@ class TestPages:
     # 8
     def test_moving_ativ_to_done_column(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -89,7 +103,7 @@ class TestPages:
     # 9
     def test_block_moving_ativ(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -106,7 +120,7 @@ class TestPages:
     # 10
     def test_returns_ativ_to_doing_column(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -123,7 +137,7 @@ class TestPages:
     # 11
     def test_returns_ativ_to_todo_column(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -144,7 +158,7 @@ class TestPages:
     # 12
     def test_blocks_returns_ativ(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -166,10 +180,11 @@ class TestPages:
         voltar.click()
         time.sleep(1)
 
-    #13
+    # 13
     def test_inserting_multiples_ativs(self):
         self.chrome.get('http://127.0.0.1:5500/registerPage.html')
 
+        time.sleep(3)
         self.chrome.find_element(By.ID, 'input').send_keys('Atividade 2')
         self.chrome.find_element(By.ID, 'button').click()
         time.sleep(1)
@@ -180,10 +195,10 @@ class TestPages:
         time.sleep(1)
         self.chrome.find_element(By.CLASS_NAME, 'atividade').is_displayed()
 
-    #14
+    # 14
     def test_moving_multiples_ativs(self):
         self.chrome.get('http://127.0.0.1:5500/kanban.html')
-        time.sleep(1)
+        time.sleep(3)
 
         avancar = self.chrome.find_element(By.ID, "avancar")
         avancar.click()
@@ -209,6 +224,9 @@ class TestPages:
         avancar.click()
         time.sleep(1)
 
+    # 15
     def test_click_in_button_for_generate_document(self):
+        time.sleep(3)
         relBtn = self.chrome.find_element(By.ID, 'relBtn')
         relBtn.click()
+        time.sleep(4)
