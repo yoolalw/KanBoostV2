b3c4b5513883e1bfe20438d6e392ea141d3ca5de
yoolalw <larakemilyprofissional@gmail.com>
2026-06-19 09:37:52 -0300

    Pastas criadas + homePage feita e sem gerador de relatorios +  testes na homepage feitos #1 #2

---
+++ js/homepage.js
@@ -0,0 +1,8 @@
+function home(){
+    window.location.href = "homepage.html"
+}
+function ca(){
+    window.location.href = "registerPage.html"
+}function kan(){
+    window.location.href = "registerPage.html"
+}
---
+++ tests/test_interface_kanboost.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture(scope="class")
+def chrome(request):
+    service = webdriver.ChromeService()
+    chrome = webdriver.Chrome(service=service)
+    request.cls.chrome = chrome
+    chrome.implicitly_wait(10)
+    yield chrome
+    chrome.quit()
+
+
+@pytest.mark.usefixtures("chrome")
+class TestPages:
+
+    def setup_method(self):
+        self.chrome.get('http://127.0.0.1:5500/homepage.html')
+
+    def test_displayed_items_in_homepage(self):
+        self.chrome.find_element(By.ID, "regBtn").is_displayed()
+        self.chrome.find_element(By.ID, "relBtn").is_displayed()
+        self.chrome.find_element(By.ID, "kanBtn").is_displayed()
+
+    def test_click_in_register_button(self):
+        self.chrome.find_element(By.ID, "regBtn").click()
+        WebDriverWait(self.chrome, 4).until(
+            ec.url_to_be('http://127.0.0.1:5500/registerPage.html')
+        )
